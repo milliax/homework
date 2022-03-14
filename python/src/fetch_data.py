0ba39368892817d192157d461f184316865fd8f1
milliax <wei@sivir.pw>
2022-03-14 16:21:08 +0000

    good parsing

--- python/src/fetch_data.py
+++ python/src/fetch_data.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request as req
 import time
 import pandas as pd
+import re
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
 
@@ -43,24 +44,48 @@ def fetch_list():
     python_table = []
 
     for element in Computer_list:
-        ##print(element)
+        ## print(element)
         row_data = []
         
         row = element.find_all("td")
-        raw = str(row[1])
         
-        # Brand
-        row_data.append(raw.split("<br/>")[0].split("</a>")[1].split("\n")[0].replace(" ",""))
-        # Country
-        row_data.append(raw.split("<br/>")[2].split("\n")[0].replace(" ",""))
-        # Name
-        row_data.append(row[1].find_all("b")[0].text)
         
-        for e in row[2:6]:
+        """ Parsing country name """
+        try:
+            country = re.search("<br\/>((\w+ \w+ \w+)|(\w+ \w+)|(\w+))\n",str(row[1])).group(0)
+            country = country.split("<br/>")[1]
+            country = country.split("\n")[0]
+            row_data.append(country)
+        except:
+            row_data.append("failed to fetech")
+            print(str(row[1]))
+            print("Unsupported format - country")
+
+        """ Parsing name """
+        try:
+            row_data.append(row[1].find_all("b")[0].text)
+        except:
+            print("Unsupported format - name")
+            print(row[1])
+            row_data.append("No name")
+        
+        """ Parsing Manufactor """
+        try:
+            manufactor = re.search("<\/a> [a-zA-Z0-9_ \/.,]+\n",str(row[1])).group(0)
+            manufactor = manufactor.split("</a>")[1]
+            manufactor = manufactor.split("\n")[0]
+            row_data.append(manufactor)
+        except:
+            row_data.append("failed to fetch")
+            print(str(row[1]))
+            print("Unsupported format - manufactor")
+
+        for e in row[2:]:
             row_data.append(e)
 
+        row_data.append(row[1].find_all("a")[0].get("href"))
         python_table.append(row_data)
     
-    DataFrame = pd.DataFrame(python_table,columns=["Brand","Country","Name","cores","Rmax","Rpeak","Power"])
-    print(DataFrame)    
+    DataFrame = pd.DataFrame(python_table,columns=["country","Name","Manufactor","cores","Rmax","Rpeak","Power","link"])
+    print(DataFrame)
     
